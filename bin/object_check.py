9514048d61627332d70288b02767ef8c018044b5
poemdistance <poemdistance@gmail.com>
2021-03-03 09:04:30 +0800

    bak

--- bin/object_check.py
+++ bin/object_check.py
@@ -36,9 +36,9 @@ class TemplateMismatchError( Exception ):
 
         if logmsg is None and len(path) > 0:
             path = 'root.' + '.'.join( str(i) for i in path )
-            logmsg = 'Missing {0} field: "{1}.{2}"'.format(declaration, path, field )
+            logmsg = 'Missing {0} field: {1}.{2} type:{3}'.format(declaration, path, field, type(field) )
         elif logmsg is None:
-            logmsg = 'Missing {0} field: "{1}"'.format(declaration, field )
+            logmsg = 'Missing {0} field: {1} type:{2}'.format(declaration, field, type(field) )
 
         super().__init__( logmsg )
 
@@ -79,6 +79,13 @@ class DependencyCheckFailed( Exception ):
 
         super().__init__( logmsg )
 
+class AppearForbiddenFieldErr( Exception ):
+    def __init__( self, forbid_field, dependency, path ):
+        prefix = 'root'
+        for node in path: prefix += '.'+str(node)
+        logmsg = 'Appear forbidden field: {0}.{1} when dependency:{2} is not satisfy'.format(prefix, forbid_field, dependency)
+        super().__init__( logmsg )
+
 class TemplateParser( object ):
 
     def __init__( self, log=print ):
@@ -105,40 +112,55 @@ class TemplateParser( object ):
             pass
         return string
 
-    def str_parser( self, key, value):
-        if value in SupportType:
-            key = [self.try_to_dict(key)]
-            if type(key[0]) == dict:
-                self.str_2_dict.update( key[0] )
-            self.target_type_list[value].extend(key)
+    # 检查模板规则, 不允许内外层出现相邻声明
+    def template_rule_check( self,  outer_declaration, field ):
+        for item, dec in field.items():
+            if dec in SupportType:
+                raise TemplateErr('Conflict declaration between <{0}> and <{1}>:\n[2]. {2}:<{3}> \n[2]. {4}:<{5}>'.\
+                        format(dec, outer_declaration, item, dec, field, outer_declaration, dec, outer_declaration))
+
+    def str_parser( self, field, declaration):
+
+        if declaration in SupportType:
+
+            field = self.try_to_dict(field)
+
+            if type(field) == dict:
+
+                self.template_rule_check( declaration, field )
+                self.str_2_dict.update( field )
+
+            self.target_type_list[declaration].extend([field])
             return
 
-        self.template_error( value )
+        self.template_error( declaration )
 
-    def dict_parser( self, key, value ):
-        if len(value) != 1:
-            return self.template_error( value )
+    def dict_parser( self, field, declaration_dict ):
+        if len(declaration_dict) != 1:
+            return self.template_error( declaration_dict)
 
-        return self.select_dict_parser( key, value )
+        return self.select_dict_parser( field, declaration_dict )
 
-    def select_dict_parser( self, key, dict_obj ):
-        for k, v in dict_obj.items():
-            if k not in SupportType:
-                raise TemplateErr(dict_obj)
+    def select_dict_parser( self, field, declaration_dict ):
+        for declaration, value in declaration_dict.items():
+            if declaration not in SupportType:
+                raise TemplateErr(declaration)
 
-            key = self.try_to_dict(key)
+            field = self.try_to_dict(field)
 
             # key 可以展开成dict的,保存进str_2_dict, 后期有用
-            if type(key) == dict:
-                self.str_2_dict.update( key )
+            if type(field) == dict:
+                self.template_rule_check( declaration, field )
+                self.str_2_dict.update( field )
 
             # tuple 可以作为key,不需要转成字符串
-            if type(key) == tuple:
-                self.target_type_list[k].append({ key: dict_obj[k] })
+            if type(field) == tuple:
+                # { Appear: 2 } , Appear 为 declaration_dict, Appear 为 declaration
+                self.target_type_list[declaration].append({ field: declaration_dict[declaration] })
                 continue
 
             # dict不可以作为key,需要转成字符串
-            self.target_type_list[k].append({ json.dumps(key): dict_obj[k] })
+            self.target_type_list[declaration].append({ json.dumps(field): declaration_dict[declaration] })
 
     def template_error( self, key, value ):
         raise TemplateErr("declaration error in key:'{0}' value:'{1}'".format(key, value))
@@ -156,9 +178,9 @@ class TemplateParser( object ):
 
     def parse_template( self, template ):
 
-        for key, value in template.items():
-            parser = self.select_declaration_parser( value )
-            parser( key, value )
+        for field, declaration in template.items():
+            parser = self.select_declaration_parser( declaration )
+            parser( field, declaration )
 
     def print_target_field( self ):
         self.log('\nDeclaration filed:\n')
@@ -172,13 +194,21 @@ class ObjChecker( TemplateParser ):
         self.log = log
         pass
 
-    def copy_list_and_append( self, list_obj, value ):
+    def copy_and_add( self, obj, value ):
 
-        result = []
-        for item in list_obj:
-            result.append( item )
+        if type(obj) == list:
+            result = []
+            add = result.append
+        elif type(obj) == set:
+            result = set()
+            add = result.add
+        else:
+            raise
 
-        result.append( value )
+        for item in obj:
+            add( item )
+
+        add( value )
         return result
 
     def is_iterable( self, obj ):
@@ -258,7 +288,7 @@ class ObjChecker( TemplateParser ):
             if field not in sub_compare_obj:
                 self.print_message(Optional, current_path, field, found=False)
             else:
-                local_path = self.copy_list_and_append( current_path, field )
+                local_path = self.copy_and_add( current_path, field )
                 self.is_sub_satisfy( compare_obj, field, path=local_path)
                 self.print_message( Optional, current_path, field )
 
@@ -280,7 +310,7 @@ class ObjChecker( TemplateParser ):
             if field not in sub_compare_obj:
                 raise TemplateMismatchError( Required, current_path, field )
 
-            local_path = self.copy_list_and_append( current_path, field )
+            local_path = self.copy_and_add( current_path, field )
             self.is_sub_satisfy( compare_obj, field, path=local_path )
 
     def required_tuple_field_checker( self, compare_obj, required_tuple_field, current_path ):
@@ -333,7 +363,7 @@ class ObjChecker( TemplateParser ):
             if type(target) == tuple: continue
 
             for field in found_field:
-                local_path = self.copy_list_and_append( current_path, field )
+                local_path = self.copy_and_add( current_path, field )
                 self.is_sub_satisfy( compare_obj, field, path=local_path )
 
     def appear_checker( self, template, compare_obj, current_path ):
@@ -342,23 +372,33 @@ class ObjChecker( TemplateParser ):
     def dependency_cheker( self, compare_obj, dependency, current_path ):
 
         sub_dependency = self.get_sub_dependency( dependency, current_path )
+        sub_compare_obj = self.get_sub_compare_obj(compare_obj, current_path)
 
         if type(sub_dependency) in CommonType:
-            sub_compare_obj = self.get_sub_compare_obj(compare_obj, current_path)
             if sub_dependency not in sub_compare_obj:
                 raise DependencyCheckFailed( compare_obj, dependency, current_path, sub_dependency )
             return
 
-        self.check_type( sub_dependency, [ dict ] )
+        if type(sub_dependency) == set:
+            local_dependency = { i for i in sub_dependency if i not in sub_compare_obj }
+            for key in sub_dependency:
+                if key not in sub_compare_obj:
+                    raise DependencyCheckFailed( compare_obj, sub_dependency, current_path, local_dependency )
+            return
+
+        self.check_type( sub_dependency, [ dict, set ] )
 
         for k, v in sub_dependency.items():
-            sub_compare_obj = self.get_sub_compare_obj(compare_obj, current_path)
             if not self.is_iterable(sub_compare_obj) or k not in sub_compare_obj:
                 raise DependencyCheckFailed( compare_obj, dependency, current_path, k )
 
-            if type(v) == dict:
-                local_path = self.copy_list_and_append( current_path, k )
+            if type(v) in CommonType and v != sub_compare_obj[k]:
+                raise DependencyCheckFailed( compare_obj, dependency, current_path, k, err_type='condition err' )
+
+            if type(v) in [ dict, set ]:
+                local_path = self.copy_and_add( current_path, k )
                 self.dependency_cheker( compare_obj, dependency, local_path )
+
 
     def relyon_common_field_checker( self, compare_obj, relyon_field, current_path ):
 
@@ -376,14 +416,35 @@ class ObjChecker( TemplateParser ):
 
             self.print_message( RelyOn, current_path, field )
 
+    def forbid_appear_field( self, compare_obj, forbid_field, dependency, current_path ):
+
+        sub_compare_obj = self.get_sub_compare_obj( compare_obj, current_path )
+
+        if type(forbid_field) in CommonType and forbid_field in sub_compare_obj:
+            raise AppearForbiddenFieldErr( forbid_field, dependency, current_path )
+
+        forbid_field = self.try_to_dict( forbid_field )
+        if self.is_iterable( forbid_field ):
+            for field in forbid_field:
+                self.log('forbid_field: {0}'.format(field))
+                if field in sub_compare_obj:
+                    raise AppearForbiddenFieldErr( field, dependency, current_path )
+                return
+
     def relyon_dict_field_checker( self, compare_obj, relyon_field, current_path ):
 
         for field, dependency in relyon_field.items():
 
             target_field = self.try_to_dict( field )
 
-            self.dependency_cheker( compare_obj, dependency, [] )
-            # self.log('Dependency:{0} check pass'.format(dependency))
+            try:
+                self.dependency_cheker( compare_obj, dependency, [] )
+                self.log('Dependency:{0} check pass'.format(dependency))
+            except DependencyCheckFailed as e:
+                # TODO 检查失败应该看下目标字段存在性,按理来说不应该存在
+                self.log('Dependency:{0} is not satisfy '.format(dependency))
+                self.forbid_appear_field( compare_obj, field, dependency, current_path )
+                continue
 
             if type(target_field) in CommonType:
                 self.relyon_common_field_checker( compare_obj, target_field, current_path )
@@ -405,7 +466,7 @@ class ObjChecker( TemplateParser ):
                     self.print_message( RelyOn, current_path, k )
                     continue
 
-                local_path = self.copy_list_and_append( current_path, k )
+                local_path = self.copy_and_add( current_path, k )
                 self.is_sub_satisfy( compare_obj, k, path=local_path )
 
     def relyon_checker( self, template, compare_obj, current_path ):
@@ -476,8 +537,15 @@ def main():
                         'second-depth': {
                             'third-depth': Required,
                             }
-                        }): Optional,
-                    }
+                        }): { RelyOn: { # FIXME 这里检测失败了
+                            "more-than-one-depth":{
+                                'second-depth':{
+                                    'third-depth': 1
+                                    }
+                                }
+                            } } ,
+                    },
+
                 }): Required,
 
             json.dumps({
@@ -492,23 +560,28 @@ def main():
                         }): Required,
 
                     json.dumps({
-                        'appear': {
-
-                            },
-
-                        'appear2': {
-
-                            }
+                        'appear': { },
+                        'appear2': { },
 
                         }): { Appear : 1 }
                     },
 
-                'second-appear': {
-
-                    }
-
                 }) : { Appear : 1  },
 
+            json.dumps({
+                'appear-relyon':{
+                    'test':     Optional,
+                    }
+                }):
+                { RelyOn :
+                    {
+                        'first-appear': {
+                            'required-in-appear': {
+                                # 'good', 'study', 'day', 'up'
+                                }
+                            }
+                    }
+                }
             }
 
     compare_obj = {
@@ -525,7 +598,7 @@ def main():
                     'third-depth': 1,
                     }
             },
-            
+
             'more-than-one-depth2': {
                 'second-depth': {
                     'third-depth': 1,
@@ -540,6 +613,10 @@ def main():
                 'appear': {
                     'test': 2
                     },
+                },
+
+            'appear-relyon': {
+
                 },
         }
 
--- bin/test.py
+++ bin/test.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
 #-*-coding:utf8-*-
 
-class test(object):
-    def __init__(self):
-        pass
+s = { 1, 2, 3, 4 }
+l = [1, 2, 3, 5, 6, 76]
 
-d = {}
-
-d[test()] = 1
+print({ i for i in s if i not in l })
